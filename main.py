5600bddaf11fadde5a22cdf93c2a94b4cbc63dd6
gwest-II <gwest0239@gmail.com>
2022-02-25 20:02:55 +0300

    Маггический метод сравненеия

--- main.py
+++ main.py
@@ -19,8 +19,28 @@ class Student:
         else:
             return 'Error'
 
+    def average_grades(self):
+        if len(self.grades.values()) > 0:
+            return sum(list(self.grades.values())[0]) / len(list(self.grades.values())[0])
+        else:
+            return 0
+
+    def __str__(self):
+        return f'Name:{self.name}\nSurname:{self.surname}\nGPA:{self.average_grades()}\n' \
+               f'Курсы в процессе изучения:{self.courses_in_progress}\n' \
+               f'Завершенные курсы:{self.finished_courses}'
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            if self.average_grades() < other.average_grades():
+                return True
+            else:
+                return False
+        else:
+            pass
 
 class Mentor:
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
@@ -28,6 +48,7 @@ class Mentor:
 
 
 class Reviewer(Mentor):
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -38,40 +59,60 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
     def __str__(self):
-        some_reviewer = f'Name:{self.name}\nSurname:{self.surname}'
-        return some_reviewer
+        return f'Name:{self.name}\nSurname:{self.surname}'
 
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.score = {}
-        self.grade_score = []
 
     def gpa(self):
-        self.grade_score =self.score.values() / len(self.score)
+        if len(self.score.values()) > 0:
+            return sum(list(self.score.values())[0]) / len(list(self.score.values())[0])
+        else:
+            return 0
 
     def __str__(self):
-        some_lecturer = f'Name:{self.name}\nSurname:{self.surname}\nGPA:{self.grade_score}'
-        return some_lecturer
+        return f'Name:{self.name}\nSurname:{self.surname}\nGPA:{self.gpa()}'
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            if self.gpa() < other.gpa():
+                return True
+            else:
+                return False
+        else:
+            pass
+
 
 best_student = Student('Ruoy', 'Eman', 'male')
 best_student.courses_in_progress += ['Python']
+cool_student = Student('Peter', 'Parker', 'male')
+cool_student.courses_in_progress += ['Python']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+cool_reviewer.rate_hw(cool_student, 'Python', 8)
+cool_reviewer.rate_hw(cool_student, 'Python', 7)
 
 cool_lecturer = Lecturer('Pop', 'Bronson')
 cool_lecturer.courses_attached += ['Python']
+
+best_lecturer = Lecturer('Robert', 'Bruce')
+best_lecturer.courses_attached += ['Python']
+best_student.lecturer_score(best_lecturer, 'Python', 10)
+cool_student.lecturer_score(best_lecturer,'Python', 10)
 best_student.lecturer_score(cool_lecturer, 'Python', 9)
 best_student.lecturer_score(cool_lecturer, 'Python', 8)
 
-some_reviewer = cool_reviewer
-some_lecturer = cool_lecturer
 print(best_student.grades)
 print(cool_lecturer.score)
-print(some_lecturer)
+print(cool_student)
+print(cool_lecturer < best_lecturer)
+print(best_student < cool_student)
