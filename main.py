210ae09aea0d7fbc5e9feebc8ae35ccfb6ff1cde
gwest-II <gwest0239@gmail.com>
2022-03-02 20:31:24 +0300

    task4

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Student:
+
+    student_list = []
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -6,7 +9,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.student_list = []
+        Student.student_list.append(self)
 
     def add_courses(self, course_name):
         self.finished_course.append(course_name)
@@ -19,6 +22,16 @@ class Student:
                 lecturer.score[course] = [score]
         else:
             return 'Error'
+
+    @staticmethod
+    def average_grade_student(student_list, course):
+        s = 0
+        counter = 0
+        for student in student_list:
+            if course in student.courses_in_progress:
+                s += student.average_grades()
+                counter += 1
+        return s/counter
 
     def average_grades(self):
         if len(self.grades.values()) > 0:
@@ -64,15 +77,29 @@ class Reviewer(Mentor):
 
 
 class Lecturer(Mentor):
+
+    lecturer_list = []
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.score = {}
+        Lecturer.lecturer_list.append(self)
 
     def gpa(self):
         if len(self.score.values()) > 0:
             return sum(list(self.score.values())[0]) / len(list(self.score.values())[0])
         else:
             return 0
+
+    @staticmethod
+    def average_score_lecturer(lecturer_list, course):
+        s = 0
+        counter = 0
+        for lecturer in lecturer_list:
+            if course in lecturer.courses_attached:
+                s += lecturer.gpa()
+                counter += 1
+        return s/counter
 
     def __str__(self):
         return f'Name:{self.name}\nSurname:{self.surname}\nGPA:{self.gpa()}'
@@ -107,11 +134,19 @@ cool_lecturer.courses_attached += ['Python']
 
 best_lecturer = Lecturer('Robert', 'Bruce')
 best_lecturer.courses_attached += ['Python']
+
 best_student.lecturer_score(best_lecturer, 'Python', 10)
 cool_student.lecturer_score(best_lecturer,'Python', 10)
 best_student.lecturer_score(cool_lecturer, 'Python', 9)
 best_student.lecturer_score(cool_lecturer, 'Python', 8)
 
+mentor_first = Mentor('Some', 'First')
+mentor_first.courses_attached += ['Python']
+mentor_second = Mentor('Richard', 'Cent')
+mentor_second.courses_attached += ['Python']
+
+print(Student.average_grade_student(Student.student_list, 'Python'))
+print(Lecturer.average_score_lecturer(Lecturer.lecturer_list, 'Python'))
 print(best_student.grades)
 print(cool_lecturer.score)
 print(cool_student)
